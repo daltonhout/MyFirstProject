43147203b08837de51c45e9edab601ce9a000a54
daltonhout <89536212+daltonhout@users.noreply.github.com>
2021-08-26 10:46:39 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,25 @@
+/*
+Dalton Hout
+C++ FA21
+Sept. 1
+Lab 1 Introduction to C++ Programming
+Description: This lab will serve as a demonstration of your
+ability to download a project from GitHub, compile and run the
+project, upload a project to GitHub, and submit a project
+to Google Classroom. This lab assumes you have downloaded and
+installed an IDE (integrated development environment) and have
+also installed GitHub desktop. If you have not met the assumptions
+for this lab, the steps you need to complete the lab may vary from
+what is here. The goals are for you to download a project, compile
+and run that project, and to upload your own project. If you meet
+the goals, you will get the points for this lab. Follow the directions
+in this lab and complete the tasks. If you have questions, please ask.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Favorite Color: Blue" << std::endl;
+}
